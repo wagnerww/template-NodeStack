8875e45506e8783b0e26442b6b9556519215bacf
wagner <wagnerricardonet@gmail.com>
2019-07-10 00:06:00 -0300

    chore:configurando o redis e filas

---
+++ src/app/controllers/agendamentoFilas.js
@@ -0,0 +1,120 @@
+const filasModel = require("../models/Filas");
+const response = require("../../config/responsePattern");
+
+class enderecosUsuarioController {
+  async index(req, res, next) {
+    try {
+      const filas = await filasModel.query().select();
+
+      response.statusCode = 200;
+      response.data = filas;
+      next(response);
+      return;
+    } catch (error) {
+      response.statusCode = 500;
+      response.message = error.message;
+      next(response);
+      return;
+    }
+  }
+
+  async show(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        response.statusCode = 400;
+        response.message = "É necessário informar o id";
+        next(response);
+        return;
+      }
+
+      const fila = await filasModel.query().findById(id);
+
+      response.statusCode = 200;
+      response.data = fila;
+      next(response);
+      return;
+    } catch (error) {
+      response.statusCode = 500;
+      response.message = error.message;
+      next(response);
+      return;
+    }
+  }
+
+  async store(req, res, next) {
+    try {
+      const { body } = req;
+
+      const fila = await filasModel.query().insert(body);
+
+      response.statusCode = 200;
+      response.data = fila;
+      next(response);
+      return;
+    } catch (error) {
+      response.statusCode = 500;
+      response.message = error.message;
+      next(response);
+      return;
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+      const { body, params } = req;
+      const { id } = params;
+
+      if (!id) {
+        response.statusCode = 400;
+        response.message = "É necessário informar o id!";
+        next(response);
+        return;
+      }
+
+      const fila = await filasModel.query().updateAndFetchById(id);
+
+      response.statusCode = 200;
+      response.data = fila;
+      next(response);
+      return;
+    } catch (error) {
+      response.statusCode = 500;
+      response.message = error.message;
+      next(response);
+      return;
+    }
+  }
+
+  async destroy(req, res, next) {
+    try {
+      const { params } = req;
+      const { id } = params;
+
+      if (!id) {
+        response.statusCode = 400;
+        response.message = "É necessário informar o id e o usuário!";
+        next(response);
+        return;
+      }
+
+      const fila = await filasModel
+        .query()
+        .delete()
+        .where({ id });
+
+      response.statusCode = 200;
+      response.data = fila;
+      next(response);
+      return;
+    } catch (error) {
+      response.statusCode = 500;
+      response.message = error.message;
+      next(response);
+      return;
+    }
+  }
+}
+
+module.exports = new enderecosUsuarioController();
---
+++ src/app/jobs/emails.js
@@ -0,0 +1,22 @@
+const redis = require("../conf/redis");
+const schedule = require("node-schedule");
+
+let exec = 0;
+persistir = async () => {
+  await redis.smembers("sendEmail", function(err, values) {
+    if (!err)
+      for (i in values) {
+        let value = values[i];
+        let email = JSON.parse(value);
+
+        redis.SREM("sendEmail", value);
+      }
+  });
+};
+
+const job = schedule.scheduleJob("0-59/5 * * * * *", async date => {
+  exec += 1;
+  await persistir();
+
+  console.log(`execução número:${exec}, hora:${date}`);
+});
