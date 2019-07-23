4b88056119b6464ba2db2fcc3ac615e665129ba0
“wagnerww” <wagnerricardonet@gmail.com>
2019-07-23 16:08:39 -0300

    chore:configurando o host da base

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 module.exports = {
   client: "postgresql",
   connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS
